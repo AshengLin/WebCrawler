80bc82105113a850a320539773bb757d1066eb0d
Derek <a0988238060@gmail.com>
2021-10-30 23:04:03 +0800

    feature: select, find siblings

--- Crawler.py
+++ Crawler.py
@@ -9,7 +9,7 @@ url = "https://www.ptt.cc/bbs/iOS/index.html"
 # F12 > Network > index.html > headers > request headers (使用者瀏覽網頁通常發出的相關資訊) > user-agent
 # 要讓網站認為你是普通使用者
 request = req.Request(url, headers={
-    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36"
 })
 
 # with req.urlopen(url) as response:  # 原本放url，改request 附加資訊
@@ -31,3 +31,16 @@ for title in all_titles:
         with open('./PPT_IOS_titles.csv', 'a', newline='\n') as myfile:
             writer = csv.writer(myfile)
             writer.writerow([title.a.string])
+            print(title.a.string)  # string
+            print(title.a['href'])  # 屬性值
+
+
+root.find_all('script', limit=2)  # 限制數量
+result = root.find('a', href="/bbs/iOS/M.1635604231.A.980.html")  # 找到該位置
+result.find_parent('div')  # 往回找父節點，找到div部分
+root.select('div')  # 篩選標籤 label = div部分
+root.select('.item')  # 篩選class = item部分
+
+test = root.find('title')
+test.find_previous_siblings("meta")  # 前節點
+test.find_next_siblings("link", limit=1)  # 後節點
